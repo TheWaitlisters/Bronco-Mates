c5bb561c715a40e3cbd97d199ddb3fcc1c978b11
ongjd99 <ongjd99@gmail.com>
2022-02-22 18:21:10 -0800

    Made johnny function return something cooler

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/Johnny")
-def johnnyA3():
-    return "<p> I hope this is what you do for A3 part 3 otherwise i dunno </p>"
+@app.route("/Johnny/<name>")
+def johnnyA3(name):
+    return f'I hope this is what you do for A3, also hi {name}'

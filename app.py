8939505794021d9f27a3d8b06cb176a80f5e7e13
AnaEstrada1996 <ana_estrada1@yahoo.com>
2022-02-24 12:45:37 -0800

    Added my part for A3

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ def hello_world():
 @app.route("/Johnny/<name>")
 def johnnyA3(name):
     return f'I hope this is what you do for A3, also hi {name}'
+
+@app.route("/Ana/<name>")
+def AnaA3(name):
+    return f'This is my part for A3 {name}'

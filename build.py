c3857c570c3fdd9fe82eb65627dc58fab7a2ab0a
ongjd99 <47126904+ongjd99@users.noreply.github.com>
2022-03-01 17:04:52 -0800

    Getting rid of Flask-PyMongo so someone else can do it

--- build.py
+++ build.py
@@ -17,6 +17,5 @@ default_task = "publish"
 def set_properties(project):
     project.build_depends_on("mockito")
     project.build_depends_on("flask")
-    project.build_depends_on("Flask-PyMongo")
     project.build_depends_on("Pillow")
 
